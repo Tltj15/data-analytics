c5976996d5f0c25ad5e0c43e86f108843e113e2f
Tracy Arista <tracyarista@gmail.com>
2026-04-13 15:02:16 -0500

    add queries of actor and film tables

--- week-2/Ex1B_first_query.sql
+++ week-2/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 sakila is a database used to collect information about database design and relationships for use by mySQL
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [number] records
+SELECT * FROM film; -- Retrieved [number] records
