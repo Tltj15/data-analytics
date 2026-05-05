924451f516934adecf0ef8a169427bb2a48dfcc5
Tracy Arista <tracyarista@gmail.com>
2026-05-05 18:21:09 -0500

    add

---
+++ week-5/Ex1B_variable_examples.py
@@ -0,0 +1,42 @@
+# Customer ID
+id_num = 1
+
+#Customers name
+name = 'John Doe' # I added a last name, even through it didnt specify 'full name'
+
+#Customer gender
+gender = 'male'
+
+#Customer date of birth
+birth_date = 01/02/2002 # I did standard date of birth no format was given or what order 
+
+#Driver’s license number
+license_num = 'A123456' # license numbers start with a letter and numbers usually 3 sets of 4
+
+#Auto policy number
+policy_num = '2222-spf-34' # policy numbers usally have a mix of number and letters
+
+# my own variables for where I was born
+name = 'Tracy'
+city = 'Chicago'
+state = 'Illinois'
+
+# a) What is the full list of reserved words that can’t be used for variable names?
+#key words can't use in python : (*only three words are have a capital first letter)
+# *False  def	if	raise
+# *None	 del	import	return
+# *True  elif	in	try
+#and	else	is	while
+#as  except  lambda 	with
+#assert 	finally 	nonlocal	yield
+#break	for  not
+#class	form	or
+#continue	global	pass
+
+
+# b) Pick 5 of these words and review the explanation for how it is used as a keyword in Python. Add these 5 definitions as # comments to your exercise document. Put ^^ around any terms that you are not familiar with
+# return-returns the value back and used to exit a function, it also lets you reuse it 
+#^^ def- used to define a function, can be resued multiple times instead of rewriting it 
+#^^ raise- used to raise an error on purpose
+#^^ while- loops a code as long as it is true
+#^^ class- create a blueprint for something 
