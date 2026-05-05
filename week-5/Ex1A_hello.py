f123ab0c50a61747978872d3ff0b16c2b77dd855
Tracy Arista <tracyarista@gmail.com>
2026-05-05 12:23:55 -0500

    add

---
+++ week-5/Ex1A_hello.py
@@ -0,0 +1,17 @@
+print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+# 'Hello world printed twice because its using the first print('Hello world!) and the print(message)
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+print(dollars + cents)
+# the results generated the two hello worlds again with the combination of dollars and cents as 3.5 it dropped the zero at end of .50
+cents = cents + .25
+print(dollars + cents)
+# the result added the .25 to the .50 and gave 3.75
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + ' ' + c_str)
